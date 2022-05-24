1d2ccee4d8d9e351440694ef28092881281b1468
ChrisWu1997 <rundi.wu@columbia.edu>
2022-05-24 21:30:57 +0000

    add visualization

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,29 +1,57 @@
-import torch
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 import h5py
+import trimesh
+from trimesh.smoothing import filter_laplacian
+from mcubes import marching_cubes
+from skimage import morphology
 
 
 def load_data_fromH5(path, smooth=True, only_finest=False):
-    voxel_list = []
+    shape_list = []
     with h5py.File(path, 'r') as fp:
         n_scales = fp.attrs['n_scales']
         if only_finest:
-            voxel = fp[f'scale{n_scales - 1}'][:]
-            return voxel
+            shape = fp[f'scale{n_scales - 1}'][:]
+            return shape
 
         for i in range(n_scales):
-            voxel = fp[f'scale{i}'][:].astype(np.float)
+            shape = fp[f'scale{i}'][:].astype(np.float)
 
             if smooth:
-                voxel = gaussian_filter(voxel, sigma=0.5)
-                voxel = np.clip(voxel, 0.0, 1.0)
-            voxel_list.append(voxel)
+                shape = gaussian_filter(shape, sigma=0.5)
+                shape = np.clip(shape, 0.0, 1.0)
+            shape_list.append(shape)
     
-    if voxel_list[0].shape[0] > voxel_list[1].shape[0]:
-        voxel_list = voxel_list[::-1]
+    if shape_list[0].shape[0] > shape_list[1].shape[0]:
+        shape_list = shape_list[::-1]
 
-    return voxel_list
+    return shape_list
+
+
+def voxelGrid2mesh(shape, laplacian=0, color=None):
+    shape = np.pad(shape, 1)
+    vertices, faces = marching_cubes(shape, 0.5)
+    mesh = trimesh.Trimesh(vertices, faces)
+    if color is not None:
+        mesh.visual.face_colors = np.array([color]).repeat(len(mesh.faces), 0)
+    if laplacian > 0:
+        mesh = filter_laplacian(mesh, iterations=laplacian)
+    return mesh
+
+
+def get_biggest_connected_compoent(voxels):
+    labels, num = morphology.label(voxels, return_num=True)
+    max_ind = 1
+    max_count = 0
+    for l in range(1, num):
+        count = np.sum(labels == l)
+        if count > max_count:
+            max_count = count
+            max_ind = l
+    mask = labels == max_ind
+    voxels[~mask] = 0
+    return voxels
 
 
 def test():
